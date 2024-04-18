5b63880ef2f0ba64bdb2844f7858455b05c1b94c
PrathikshaKairen <156254691+PrathikshaKairen@users.noreply.github.com>
2024-04-17 21:33:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,39 @@
-// functionality for showing/hiding the comments section
+document.addEventListener('DOMContentLoaded', function() {
+  const showHideBtn = document.querySelector('.show-hide');
+  const commentWrapper = document.querySelector('.comment-wrapper');
 
-const showHideBtn = document.querySelector('.show-hide');
-const commentWrapper = document.querySelector('.comment-wrapper');
+  showHideBtn.addEventListener('click', function() {
+    const isExpanded = commentWrapper.hidden;
+    commentWrapper.hidden = !isExpanded;
+    showHideBtn.setAttribute('aria-expanded', !isExpanded);
+    showHideBtn.textContent = isExpanded ? 'Show comments' : 'Hide comments';
+  });
 
-commentWrapper.style.display = 'none';
+  const form = document.querySelector('.comment-form');
+  const nameField = document.querySelector('#name');
+  const commentField = document.querySelector('#comment');
+  const list = document.querySelector('.comment-container');
 
-showHideBtn.onclick = function() {
-  let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
-    showHideBtn.textContent = 'Hide comments';
-    commentWrapper.style.display = 'block';
-  } else {
-    showHideBtn.textContent = 'Show comments';
-    commentWrapper.style.display = 'none';
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    submitComment();
+  });
+
+  function submitComment() {
+    const listItem = document.createElement('li');
+    const namePara = document.createElement('p');
+    const commentPara = document.createElement('p');
+    const nameValue = nameField.value;
+    const commentValue = commentField.value;
+
+    namePara.textContent = nameValue;
+    commentPara.textContent = commentValue;
+
+    list.appendChild(listItem);
+    listItem.appendChild(namePara);
+    listItem.appendChild(commentPara);
+
+    nameField.value = '';
+    commentField.value = '';
   }
-};
-
-// functionality for adding a new comment via the comments form
-
-const form = document.querySelector('.comment-form');
-const nameField = document.querySelector('#name');
-const commentField = document.querySelector('#comment');
-const list = document.querySelector('.comment-container');
-
-form.onsubmit = function(e) {
-  e.preventDefault();
-  submitComment();
-};
-
-function submitComment() {
-  const listItem = document.createElement('li');
-  const namePara = document.createElement('p');
-  const commentPara = document.createElement('p');
-  const nameValue = nameField.value;
-  const commentValue = commentField.value;
-
-  namePara.textContent = nameValue;
-  commentPara.textContent = commentValue;
-
-  list.appendChild(listItem);
-  listItem.appendChild(namePara);
-  listItem.appendChild(commentPara);
-
-  nameField.value = '';
-  commentField.value = '';
-}
+});
